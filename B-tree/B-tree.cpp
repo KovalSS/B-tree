1986e34b8a7b574c56443259789044db4bd12c32
Коваль <kovalss2006@gmail.com>
2025-06-21 13:20:54 +0300

    перевірка гіт ігнора

--- B-tree/B-tree.cpp
+++ B-tree/B-tree.cpp
@@ -1,8 +1,33 @@
 ﻿#include <iostream>
-using namespace std;
+#include <fstream>
+#include <filesystem>
 
-int main()
-{
-    cout << "LoL";
+using namespace std;
+int main() {
+    // Create directory if it doesn't exist
+    string folderName = "SaveData";
+
+    // Create a file outside the folder
+    ofstream outsideFile("file_outside.txt");
+    if (outsideFile.is_open()) {
+        outsideFile << "This file is outside the folder.\n";
+        outsideFile.close();
+        cout << "File 'file_outside.txt' created successfully.\n";
+    }
+    else {
+        cerr << "Failed to create 'file_outside.txt'.\n";
+    }
+
+    // Create a file inside the folder
+    ofstream insideFile(folderName + "/file_in_folder.txt");
+    if (insideFile.is_open()) {
+        insideFile << "This file is inside the folder.\n";
+        insideFile.close();
+        cout << "File '" << folderName << "/file_in_folder.txt' created successfully.\n";
+    }
+    else {
+        cerr << "Failed to create '" << folderName << "/file_in_folder.txt'.\n";
+    }
+
     return 0;
 }
